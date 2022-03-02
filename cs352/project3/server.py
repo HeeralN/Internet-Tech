9bf31c7527ce466af4259371a1352b9de404d26f
Jackie Hong <jh1523@ilab3.cs.rutgers.edu>
2022-03-02 02:53:00 -0500

    part 7

--- cs352/project3/server.py
+++ cs352/project3/server.py
@@ -13,6 +13,7 @@ else:
 
 # Start a listening server socket on the port
 sock = socket.socket()
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # TODO: Remove this?
 sock.bind(('', port))
 sock.listen(2)
 
@@ -29,6 +30,8 @@ login_form = """
 login_page = "<h1>Please login</h1>" + login_form
 # Error page for bad credentials
 bad_creds_page = "<h1>Bad user/pass! Try again</h1>" + login_form
+# Error page for bad cookies
+bad_cookie_page = "<h1>Bad cookie! Try again</h1>" + login_form
 # Successful logout
 logout_page = "<h1>Logged out successfully</h1>" + login_form
 # A part of the page that will be displayed after successful
@@ -65,7 +68,10 @@ signal.signal(signal.SIGINT, sigint_handler)
 # Read login credentials for all the users
 # Read secret data of all the users
 
-# Password database {key: (pw, secret)}
+# Cookies database {cookie_val: username}
+cookies = dict()
+
+# Password database {username: (password, secret)}
 data = dict()
 pwFile = open("passwords.txt", 'r')
 secretsFile = open("secrets.txt", 'r')
@@ -97,9 +103,19 @@ while True:
     # body = "username=bezos&password=amazon"
     # body = "username=bezos"
 
-    username = password = action = None
+    username = password = action = token = None
+    headers_to_send = ''
+
+    # Parse header for cookie
+    headers = headers.split('\r\n')
+    print(headers)
+    for field in headers:
+        param = field.split(':')
+        if param[0] == 'Cookie':
+            token = param[1].split('=')[1]
+
+    # Parse body for username, password, or action
     body = body.split('&') if body != '' else []
-    print('body', body)
     for field in body:
         param = field.split('=')
         if param[0] == 'username':
@@ -109,17 +125,25 @@ while True:
         elif param[0] == 'action':
             action = 'logout'
 
-    if username == None and password == None:  # Default login
+    if action == 'logout':
+        html_content_to_send = logout_page
+        headers_to_send = "Set-Cookie: token=; expires=Thu, 01 Jan 1970 00:00:00 GMT\r\n"
+    elif token is not None: # Header contains a cookie
+        if token in cookies: # Valid cookie
+            html_content_to_send = success_page + data[cookies[token]][1]  # success page + secret
+        else: # Invalid cookie
+            html_content_to_send = bad_cookie_page # Retry login
+    elif username == None and password == None:  # Default login
         html_content_to_send = login_page 
     elif username in data and data[username][0] == password: # Good login
         html_content_to_send = success_page + data[username][1]  # success page + secret
+        cookie_val = random.getrandbits(64)
+        cookies[str(cookie_val)] = username
+        headers_to_send = 'Set-Cookie: token=' + str(cookie_val) + '\r\n'
     elif username is None or password is None or username not in data or password != data[username][0]: # empty or incorrect fields
         html_content_to_send = bad_creds_page # Retry login
-    elif action == 'logout':
-        html_content_to_send = logout_page
     else: # Default login, should never get here
-        html_content_to_send = login_page 
-
+        html_content_to_send = login_page
 
     # You need to set the variables:
     # (1) `html_content_to_send` => add the HTML content you'd
@@ -134,7 +158,6 @@ while True:
     # (2) `headers_to_send` => add any additional headers
     # you'd like to send the client?
     # Right now, we don't send any extra headers.
-    headers_to_send = ''
 
     # Construct and send the final response
     response  = 'HTTP/1.1 200 OK\r\n'
